bce823af411ca5540c0bdc98ebabfcb6d4788f91
CaesarBrahh <julaenw@gmail.com>
2026-06-16 20:01:28 -0500

    HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        if self.props == None or len(self.props) == 0:
+            return ""
+
+        total = ""
+        first_iteration = True
+        for key in self.props.keys():
+            if not first_iteration:
+                total += " "
+            total += f'{key}="{self.props[key]}"'
+            first_iteration = False
+
+        return total
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,30 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestTextNode(unittest.TestCase):
+    def test_initialization(self):
+        node = HTMLNode()
+        self.assertEqual(node.tag, None)
+        self.assertEqual(node.value, None)
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+
+    def test_props_to_html_1(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_2(self):
+        node = HTMLNode("a", "Google", None, {"href": "https://www.google.com", "onClick": True, "id": "link"})
+        self.assertEqual(node.props_to_html(), 'href="https://www.google.com" onClick="True" id="link"')
+
+    def test_to_html(self):
+        node = HTMLNode()
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_repr(self):
+        node = HTMLNode("p", "hello world")
+        self.assertEqual(repr(node), "HTMLNode(p, hello world, None, None)")
+
+if __name__=="__main__":
+    unittest.main()
