875d3114023a27ee76122825455781b077e24673
CaesarBrahh <julaenw@gmail.com>
2026-06-25 22:14:31 -0500

    generate pages recursive

---
+++ src/generate_pages_recursive.py
@@ -0,0 +1,25 @@
+import os
+from generate_page import generate_page
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    # ensure dir_path_content exists
+    if not os.path.exists(dir_path_content):
+        raise ValueError("Directory path of content does not exist")
+
+    # store dir_path_contents
+    contents = os.listdir(dir_path_content)
+
+    for content in contents:
+        from_path = os.path.join(dir_path_content, content)
+        to_path = os.path.join(dest_dir_path, content)
+
+        if os.path.isfile(from_path):
+            generate_page(from_path, template_path, os.path.join(dest_dir_path, "index.html"))
+        else:
+            # make the to_path directory real
+            os.mkdir(to_path)
+
+            # recurse into the file
+            generate_pages_recursive(from_path, template_path, to_path)
+
+    return
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from static_to_public import static_to_public
 from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 
 def main():
     static_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__=="__main__":
     main()
